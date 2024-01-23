53c9936042b911a9f9f1ca1cad6697d73dc6f043
Mike Mollick <mollick2@gmail.com>
2024-01-23 17:20:53 -0500

    feat: replace with Astro

---
+++ tailwind.config.mjs
@@ -0,0 +1,17 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ["Inter", ...defaultTheme.fontFamily.sans],
+				serif: ["IBM Plex Serif", ...defaultTheme.fontFamily.serif],
+			  },
+		},
+	},
+	plugins: [
+		require('@tailwindcss/typography')
+	],
+}
