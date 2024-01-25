c477c87b19ddf3ebf899b601c88451d16b028fb9
Mike Mollick <mollick2@gmail.com>
2024-01-25 13:23:45 -0500

    feat: add link to product engineering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
