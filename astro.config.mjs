86214d6b37bcd8657001851ec9627f1bae0d71ba
Mike Mollick <mollick2@gmail.com>
2024-01-25 13:18:02 -0500

    feat: adding sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()]
+  site: 'https://mikemollick.com',
+  integrations: [tailwind(), mdx(), sitemap({
+    filter: (page) => page !== 'https://mikemollick.com/styleguide/',
+  })]
 });
