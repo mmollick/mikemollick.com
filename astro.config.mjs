93db2b7816758228001746505a0adfae59bc205e
Mike Mollick <mollick2@gmail.com>
2026-02-22 22:00:51 -0500

    upgrades

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mikemollick.com',
-  integrations: [tailwind(), mdx(), sitemap({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  integrations: [mdx(), sitemap({
     filter: (page) => page !== 'https://mikemollick.com/styleguide/',
   })]
 });
